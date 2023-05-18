d43f37720c2b6acd39f547c610b657ad81b51fba
Ismael Bueno <ismaelwno@gmail.com>
2023-05-18 19:07:44 +0200

    Added support for different allocators

--- include/yaml-cpp/stlemitter.h
+++ include/yaml-cpp/stlemitter.h
@@ -7,10 +7,10 @@
 #pragma once
 #endif
 
-#include <vector>
 #include <list>
-#include <set>
 #include <map>
+#include <set>
+#include <vector>
 
 namespace YAML {
 template <typename Seq>
@@ -22,29 +22,31 @@ inline Emitter& EmitSeq(Emitter& emitter, const Seq& seq) {
   return emitter;
 }
 
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::vector<T>& v) {
+template <typename T, typename Alloc>
+inline Emitter& operator<<(Emitter& emitter, const std::vector<T, Alloc>& v) {
   return EmitSeq(emitter, v);
 }
 
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::list<T>& v) {
+template <typename T, typename Alloc>
+inline Emitter& operator<<(Emitter& emitter, const std::list<T, Alloc>& v) {
   return EmitSeq(emitter, v);
 }
 
-template <typename T>
-inline Emitter& operator<<(Emitter& emitter, const std::set<T>& v) {
+template <typename T, typename Alloc>
+inline Emitter& operator<<(Emitter& emitter,
+                           const std::set<T, std::less<T>, Alloc>& v) {
   return EmitSeq(emitter, v);
 }
 
-template <typename K, typename V>
-inline Emitter& operator<<(Emitter& emitter, const std::map<K, V>& m) {
+template <typename K, typename V, typename Alloc>
+inline Emitter& operator<<(Emitter& emitter,
+                           const std::map<K, V, std::less<K>, Alloc>& m) {
   emitter << BeginMap;
   for (const auto& v : m)
     emitter << Key << v.first << Value << v.second;
   emitter << EndMap;
   return emitter;
 }
-}
+}  // namespace YAML
 
 #endif  // STLEMITTER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
